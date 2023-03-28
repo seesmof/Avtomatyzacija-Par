f9fa820b332a47a96fae16562e3c4cc02cd0c9bd
Oleh Onyshchenko <seesmwork@gmail.com>
2023-03-28 21:57:25 +0300

    update test.py

--- test.py
+++ test.py
@@ -1,6 +1,18 @@
-import pyautogui
-import time
+# import the necessary packages
+import pytesseract
+from textblob import TextBlob
+from PIL import Image
 
-while True:
-    pyautogui.press('f5')
-    time.sleep(10)
+# load the input image and convert it to grayscale
+path_to_tesseract = r"C:\Program Files\Tesseract-OCR\tesseract.exe"
+image = Image.open("input.jpg")
+
+# Provide the tesseract executable location to the pytesseract library
+pytesseract.tesseract_cmd = path_to_tesseract
+
+# Pass the image object to the image_to_string() function
+# This function will extract the text from the image
+text = pytesseract.image_to_string(image)
+
+# Display the extracted text
+print(text)
