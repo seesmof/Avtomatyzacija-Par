bafc06c609219ad3d338c34a3dfee75540e7d9a3
Oleh Onyshchenko <seesmwork@gmail.com>
2023-05-15 14:28:29 +0000

    🌿: Monday, May 15, 2023 at 5:28:29 PM Eastern European Summer Time

--- test.py
+++ test.py
@@ -1,9 +1,4 @@
-import schedule
-import webbrowser
-import pyperclip
-import datetime
-from datetime import date
-import time
-
-webbrowser.open_new_tab(
-    "joplin://x-callback-url/openFolder?id=57cebf0ff83f400591039def63b8bd70")
+import pyttsx3
+engine = pyttsx3.init()
+engine.say("Hello World!")
+engine.runAndWait()
