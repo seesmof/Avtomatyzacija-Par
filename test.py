de59b5b1bac2ab737b3e96e02c113ae340f51d0c
Oleh Onyshchenko <seesmwork@gmail.com>
2023-06-06 09:15:43 +0000

    🌿: Tuesday, June 6, 2023 at 12:15:43 PM Eastern European Summer Time

--- test.py
+++ test.py
@@ -1,9 +1,4 @@
-from elevenlabs import generate, play
-
-audio = generate(
-    text="Hi! My name is Bella, nice to meet you!",
-    voice="Bella",
-    model="eleven_monolingual_v1"
-)
-
-play(audio)
+reference_clips = [utils.audio.load_audio(p, 22050) for p in clips_paths]
+tts = api.TextToSpeech()
+pcm_audio = tts.tts_with_preset(
+    "your text here", voice_samples=reference_clips, preset='fast')
