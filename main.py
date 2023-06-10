41eeee48af1079714fe26ac3812c963c7175bf75
Oleh Onyshchenko <seesmwork@gmail.com>
2023-06-10 08:59:34 +0300

    modified library.cpython-311.pyc and main.py

--- main.py
+++ main.py
@@ -3,3 +3,4 @@ from library import *
 os.startfile(r"D:/repos/gcalendar-automation/tasks_scheduling.py")
 os.startfile(r"D:/repos/gcalendar-automation/monitor_kodi.py")
 os.startfile(r"D:/repos/gcalendar-automation/lock_await.py")
+os.startfile(r"D:/repos/python-twitchio-chat-bot/_main.py")
