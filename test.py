d3f93769064873418d776c0ab6c378adab3c24bb
Oleh Onyshchenko <seesmwork@gmail.com>
2023-09-15 09:36:57 +0300

    update tasks_scheduling.py and test.py

--- test.py
+++ test.py
@@ -1,3 +1,25 @@
-from y2t1_class_names_shortcuts import *
+import re
+import time
+import datetime
+from todoist_api_python.api import TodoistAPI
 
-webbrowser.open_new_tab("obsidian://open?vault=obsidian-main-vault&file=university%2F2%20%D0%BA%D1%83%D1%80%D1%81%2F1%20%D1%81%D0%B5%D0%BC%D0%B5%D1%81%D1%82%D1%80%2F%D0%90%D0%BB%D0%B3%D0%BE%D1%80%D0%B8%D1%82%D0%BC%D0%B8%20%D1%82%D0%B0%20%D0%A1%D1%82%D1%80%D1%83%D0%BA%D1%82%D1%83%D1%80%D0%B8%20%D0%94%D0%B0%D0%BD%D0%B8%D1%85%2F121-1%20%D0%90%D0%BB%D0%B3%D0%BE%D1%80%D0%B8%D1%82%D0%BC%D0%B8%20%D1%82%D0%B0%20%D0%A1%D1%82%D1%80%D1%83%D0%BA%D1%82%D1%83%D1%80%D0%B8%20%D0%94%D0%B0%D0%BD%D0%B8%D1%85")
+
+def fetchAndReschedule():
+    api = TodoistAPI("e3b0b2ed0642281f8f775fc954ef1567ea84537c")
+    today = time.strftime("%Y-%m-%d")
+    tasks = api.get_tasks()
+    timeRegex = r"\d{2}:\d{2}"
+    for task in tasks:
+        if task.due is not None and task.due.date == today:
+            dueTime = re.findall(timeRegex, task.due.string)[0]
+            print(dueTime)
+            print(task.content)
+
+
+fetchAndReschedule()
+
+# date format example from task object below
+# due=Due(date='2023-09-16', is_recurring=False, string='2023-09-16 20:00', datetime='2023-09-16T17:00:00Z', timezone='Europe/Kiev')
+
+# full task object below
+# Task(assignee_id=None, assigner_id=None, comment_count=0, is_completed=False, content='competitive programming group meeting', created_at='2023-09-14T13:52:27.303016Z', creator_id='45513878', description='Субота, 16 вересня · 20:00 – 21:00\n\nПосилання на відеодзвінок: [https://meet.google.com/snw-mzxn-mgs](https://meet.google.com/snw-mzxn-mgs)', due=Due(date='2023-09-16', is_recurring=False, string='2023-09-16 20:00', datetime='2023-09-16T17:00:00Z', timezone='Europe/Kiev'), id='7228425330', labels=[], order=18, parent_id=None, priority=1, project_id='2318870175', section_id=None, url='https://todoist.com/showTask?id=7228425330', sync_id=None)
