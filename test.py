86f599a5c337a00fb1e44fbbe05073df41a0791d
Oleh Onyshchenko <seesmwork@gmail.com>
2023-09-15 09:47:38 +0300

    update test.py

--- test.py
+++ test.py
@@ -9,17 +9,30 @@ def fetchAndReschedule():
     today = time.strftime("%Y-%m-%d")
     tasks = api.get_tasks()
     timeRegex = r"\d{2}:\d{2}"
+
     for task in tasks:
         if task.due is not None and task.due.date == today:
-            dueTime = re.findall(timeRegex, task.due.string)[0]
-            print(dueTime)
-            print(task.content)
+            dueTime = re.findall(timeRegex, task.due.string)
+            print(
+                f"\n{task.content} at {dueTime[0] if dueTime else 'any time'}\n")
 
 
 fetchAndReschedule()
 
-# date format example from task object below
-# due=Due(date='2023-09-16', is_recurring=False, string='2023-09-16 20:00', datetime='2023-09-16T17:00:00Z', timezone='Europe/Kiev')
+# tasks examples
 
-# full task object below
-# Task(assignee_id=None, assigner_id=None, comment_count=0, is_completed=False, content='competitive programming group meeting', created_at='2023-09-14T13:52:27.303016Z', creator_id='45513878', description='Субота, 16 вересня · 20:00 – 21:00\n\nПосилання на відеодзвінок: [https://meet.google.com/snw-mzxn-mgs](https://meet.google.com/snw-mzxn-mgs)', due=Due(date='2023-09-16', is_recurring=False, string='2023-09-16 20:00', datetime='2023-09-16T17:00:00Z', timezone='Europe/Kiev'), id='7228425330', labels=[], order=18, parent_id=None, priority=1, project_id='2318870175', section_id=None, url='https://todoist.com/showTask?id=7228425330', sync_id=None)
+# Task(
+#     is_completed=False,
+#     content='ЛК. Спеціальні Розділи Вищої Математики',
+#     description='',
+#     due=Due(date='2023-09-15', string='every other fri at 10:05'),
+#     priority=1
+# )
+
+# Task(
+#     is_completed=False,
+#     content='test priority 1',
+#     description='',
+#     due=Due(date='2023-09-15', string='15 Sep'),
+#     priority=4
+# )
