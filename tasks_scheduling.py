093979c539869a7b7f3eb9a9a0787590ce986442
Oleh Onyshchenko <seesmwork@gmail.com>
2023-09-16 11:08:05 +0300

    update tasks_scheduling.py

--- tasks_scheduling.py
+++ tasks_scheduling.py
@@ -8,6 +8,7 @@ today = date.today()
 dayName = today.strftime("%A")
 current_week = datetime.date.today().isocalendar()[1]
 weather = f"Outside its {get_weather()} degrees"
+globalTasks = []
 
 classOneTime = "08:30"
 classTwoTime = "10:05"
@@ -22,9 +23,9 @@ speak_text(weather)
 time.sleep(1)
 clear_downloads_folder()
 speak_text("Don't forget to make a daily donate.")
-time.sleep(1)
+time.sleep(2)
 news()
-time.sleep(1)
+time.sleep(4)
 mail()
 
 weekNominationStatus = ""
@@ -35,85 +36,24 @@ else:
     weekNominationStatus = "Чисельник"
     speak_text("This week is a Numerator.")
 
-if dayName != "Saturday" or dayName != "Sunday":
-    speak_text("Today's classes:")
-else:
-    speak_text("No classes today!")
-
-if dayName == "Monday":
-    speak_text(f"{ASD} Lecture at {classThreeTime}")
-    schedule.every().day.at(classThreeTime).do(ASD_lecture)
-    if weekNominationStatus == "Чисельник":
-        speak_text(f"{TY} Practice at {classFourTime}")
-        schedule.every().day.at(classFourTime).do(TY_practice)
-        speak_text(f"{SP} Lecture at {classFiveTime}")
-        schedule.every().day.at(classFiveTime).do(SP_lecture)
-    else:
-        speak_text(f"{SP} Lab at {classFourTime}")
-        schedule.every().day.at(classFourTime).do(SP_practice)
-elif dayName == "Tuesday":
-    speak_text(f"{WEB} Lab at {classOneTime}")
-    schedule.every().day.at(classOneTime).do(WEB_practice)
-    speak_text(f"{ASD} Lab at {classTwoTime}")
-    schedule.every().day.at(classTwoTime).do(ASD_practice)
-    speak_text(f"{OPI} Lab at {classFiveTime}")
-    schedule.every().day.at(classFiveTime).do(OPI_practice)
-    if weekNominationStatus == "Знаменник":
-        speak_text(f"{OPI} Practice at {classSixTime}")
-        schedule.every().day.at(classSixTime).do(OPI_practice)
-elif dayName == "Wednesday":
-    speak_text(f"{WEB} Lecture at {classTwoTime}")
-    schedule.every().day.at(classTwoTime).do(WEB_lecture)
-    if weekNominationStatus == "Чисельник":
-        speak_text(f"{TY} Lecture at {classOneTime}")
-        schedule.every().day.at(classOneTime).do(TY_lecture)
-elif dayName == "Thursday":
-    if weekNominationStatus == "Чисельник":
-        speak_text(f"{IY} Practice at {classFiveTime}")
-        schedule.every().day.at(classFiveTime).do(IY_practice)
-    else:
-        speak_text(f"{OPI} Lecture at {classTwoTime}")
-        schedule.every().day.at(classTwoTime).do(OPI_lecture)
-        speak_text(f"{IY} Lecture at {classFiveTime}")
-        schedule.every().day.at(classFiveTime).do(IY_lecture)
-elif dayName == "Friday":
-    if weekNominationStatus == "Чисельник":
-        speak_text(f"{VM} Lecture at {classTwoTime}")
-        schedule.every().day.at(classTwoTime).do(VM_lecture)
-        speak_text(f"{VM} Practice at {classThreeTime}")
-        schedule.every().day.at(classThreeTime).do(VM_practice)
-
 sunsetTime = get_sunset()
 schedule.every().day.at(sunsetTime).do(close_window)
 
 
 def fetchAndReschedule():
     api = TodoistAPI("e3b0b2ed0642281f8f775fc954ef1567ea84537c")
-    today = time.strftime("%Y-%m-%d")
     tasks = api.get_tasks()
+
+    today = time.strftime("%Y-%m-%d")
     timeRegex = r"\d{2}:\d{2}"
     tasksForToday = [
         t for t in tasks if t.due is not None and t.due.date == today]
-    classContents = {
-        "ПР. ": {
-            "Алгоритми та Структури Даних": ASD_practice,
-            "Спортивне Програмування": SP_practice,
-            "Вебтехнології та Вебдизайн": WEB_practice,
-            "Основи Програмної Інженерії": OPI_practice,
-            "Історія України": IY_practice,
-            "Теорія Ймовірності": TY_practice,
-            "Спеціальні Розділи Вищої Математики": VM_practice
-        },
-        "ЛК. ": {
-            "Алгоритми та Структури Даних": ASD_lecture,
-            "Спортивне Програмування": SP_lecture,
-            "Вебтехнології та Вебдизайн": WEB_lecture,
-            "Основи Програмної Інженерії": OPI_lecture,
-            "Історія України": IY_lecture,
-            "Теорія Ймовірності": TY_lecture,
-            "Спеціальні Розділи Вищої Математики": VM_lecture
-        }
-    }
+    for task in tasksForToday:
+        dueTime = re.findall(timeRegex, task.due.string)
+        globalTasks.append({
+            "content": task.content,
+            "due": dueTime[0] if dueTime else "any time"
+        })
 
     for task in tasksForToday:
         dueTime = re.findall(timeRegex, task.due.string)
@@ -160,9 +100,15 @@ def fetchAndReschedule():
             if task.priority in priorityLevels:
                 schedule.every(priorityLevels[task.priority]).hours.do(
                     speak_text, f"Don't forget to do: {translatedTask}")
+        if task not in globalTasks:
+            globalTasks.append({
+                "content": task.content,
+                "due": dueTime[0] if dueTime else "any time"
+            })
 
 
 schedule.every().hour.do(fetchAndReschedule)
+fetchAndReschedule()
 
 while True:
     schedule.run_pending()
