e773587e3bb3238ce20073347535c0fbe84fed01
Oleh Onyshchenko <seesmwork@gmail.com>
2023-09-18 16:45:09 +0300

    update tasks_scheduling.py

--- tasks_scheduling.py
+++ tasks_scheduling.py
@@ -16,71 +16,65 @@ classFourTime = "13:25"
 classFiveTime = "14:55"
 classSixTime = "16:45"
 
-speak_text(random.choice(informal_greetings))
-speak_text("Today is", dayName)
-speak_text(weather)
-time.sleep(1)
 clear_downloads_folder()
-speak_text("Don't forget to make a daily donate.")
-time.sleep(2)
+print("Don't forget to make a daily donate.")
 news()
-time.sleep(4)
 mail()
 
 weekNominationStatus = ""
 if current_week % 2 == 0:
     weekNominationStatus = "Знаменник"
-    speak_text("This week is a Denominator.")
+    print("This week is a Denominator.")
 else:
     weekNominationStatus = "Чисельник"
-    speak_text("This week is a Numerator.")
+    print("This week is a Numerator.")
 
 if dayName != "Saturday" or dayName != "Sunday":
-    speak_text("Today's classes:")
+    print("Today's classes:")
 else:
-    speak_text("No classes today!")
+    print("No classes today!")
 
 if dayName == "Monday":
-    speak_text(f"{ASD} Lecture at {classThreeTime}")
+    print(f"{ASD} Lecture at {classThreeTime}")
     schedule.every().day.at(classThreeTime).do(ASD_lecture)
     if weekNominationStatus == "Чисельник":
-        speak_text(f"{TY} Practice at {classFourTime}")
+        print(f"{TY} Practice at {classFourTime}")
         schedule.every().day.at(classFourTime).do(TY_practice)
-        speak_text(f"{SP} Lecture at {classFiveTime}")
+        print(f"{SP} Lecture at {classFiveTime}")
         schedule.every().day.at(classFiveTime).do(SP_lecture)
     else:
-        speak_text(f"{SP} Lab at {classFourTime}")
+        print(f"{SP} Lab at {classFourTime}")
         schedule.every().day.at(classFourTime).do(SP_practice)
 elif dayName == "Tuesday":
-    speak_text(f"{WEB} Lab at {classOneTime}")
+    print(f"{WEB} Lab at {classOneTime}")
     schedule.every().day.at(classOneTime).do(WEB_practice)
-    speak_text(f"{ASD} Lab at {classTwoTime}")
+    print(f"{ASD} Lab at {classTwoTime}")
     schedule.every().day.at(classTwoTime).do(ASD_practice)
-    speak_text(f"{OPI} Lab at {classFiveTime}")
+    print(f"{OPI} Lab at {classFiveTime}")
     schedule.every().day.at(classFiveTime).do(OPI_practice)
     if weekNominationStatus == "Знаменник":
-        speak_text(f"{OPI} Practice at {classSixTime}")
+        print(f"{OPI} Practice at {classSixTime}")
         schedule.every().day.at(classSixTime).do(OPI_practice)
 elif dayName == "Wednesday":
-    speak_text(f"{WEB} Lecture at {classTwoTime}")
+    print(f"{WEB} Lecture at {classTwoTime}")
     schedule.every().day.at(classTwoTime).do(WEB_lecture)
     if weekNominationStatus == "Чисельник":
-        speak_text(f"{TY} Lecture at {classOneTime}")
+        print(f"{TY} Lecture at {classOneTime}")
         schedule.every().day.at(classOneTime).do(TY_lecture)
 elif dayName == "Thursday":
     if weekNominationStatus == "Чисельник":
-        speak_text(f"{IY} Practice at {classFiveTime}")
+        print(f"{IY} Practice at {classFiveTime}")
         schedule.every().day.at(classFiveTime).do(IY_practice)
     else:
-        speak_text(f"{OPI} Lecture at {classTwoTime}")
+        print(f"{OPI} Lecture at {classTwoTime}")
         schedule.every().day.at(classTwoTime).do(OPI_lecture)
-        speak_text(f"{IY} Lecture at {classFiveTime}")
+        print(f"{IY} Lecture at {classFiveTime}")
         schedule.every().day.at(classFiveTime).do(IY_lecture)
 elif dayName == "Friday":
     if weekNominationStatus == "Чисельник":
-        speak_text(f"{VM} Lecture at {classTwoTime}")
+        print(f"{VM} Lecture at {classTwoTime}")
         schedule.every().day.at(classTwoTime).do(VM_lecture)
-        speak_text(f"{VM} Practice at {classThreeTime}")
+        print(f"{VM} Practice at {classThreeTime}")
         schedule.every().day.at(classThreeTime).do(VM_practice)
 
 
