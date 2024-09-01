75e9466d1b055eff3e4d6a865014cd7dedba813c
Oleh Onyshchenko <110289150+seesmof@users.noreply.github.com>
2024-09-01 21:19:19 +0300

    feat: JESUS THANK YOU JESUS HALLELUJAH JESUS THANK YOU JESUS HALLELUJAH JESUS THANK YOU JESUS HALLELUJAH

--- main.py
+++ main.py
@@ -12,11 +12,18 @@ target_project_id = "2318959463"
 now = datetime.now()
 
 
-def open_class(uri, pin):
-    print(f"Opening {uri}")
-    webbrowser.open(uri)
-    if pin:
-        pyperclip.copy(pin)
+def open_class(data):
+    class_name, class_type, class_time, class_uri, class_pin = data.values()
+    class_type_string = "Practice" if class_type == "P" else "Lecture"
+
+    print(f"Opening {class_name} {class_type_string} on {class_time}")
+    webbrowser.open(class_uri)
+
+    note_name=f"{now.strftime("%d-%m-%Y")}_{class_name}-{class_type_string}"
+    pyperclip.copy(note_name)
+
+    if class_pin:
+        pyperclip.copy(class_pin)
 
 
 def read_api_key():
@@ -43,17 +50,28 @@ with open(os.path.join(current_dir, "info.json"), "r", encoding="utf-8") as f:
 
 for current_class in classes:
     class_time = current_class.due.string.split(" ")[-1]
-    class_name = current_class.content
+    task_full_name = current_class.content
+    class_type, class_name = task_full_name.split(" ")
 
-    class_type, course_name = class_name.split(" ")
-
-    course_info = courses_info[course_name]
+    course_info = courses_info[class_name]
     uri = course_info[class_type]["uri"]
     pin = course_info[class_type].get("pin", "")
 
-    schedule.every().day.at(class_time).do(open_class, uri, pin)
+    class_info = {
+        "name": class_name,
+        "type": class_type,
+        "time": class_time,
+        "uri": uri,
+        "pin": pin,
+    }
+
+    class_hour, class_minute = class_time.split(":")
+    class_minute = int(class_minute)-3
+    class_time = f"{class_hour}:{class_minute:02d}"
+
+    schedule.every().day.at(class_time).do(open_class, class_info)
     print(
-        f"{course_name} {'Lecture' if class_type == 'L' else 'Practice'} at {class_time}"
+        f"{class_name} {'Lecture' if class_type == 'L' else 'Practice'} at {class_time}"
     )
 
 while 1:
