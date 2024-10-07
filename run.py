6221bff78961bbdcaa1b96bb93f3834155683337
Oleh Onyshchenko <seesmwork@gmail.com>
2024-10-07 17:14:39 +0300

    feat: ISUSE djakuju shcho dopomig meni zakinchyty z cym

--- run.py
+++ run.py
@@ -1,15 +1,15 @@
-import pyperclip
 from todoist_api_python.api import TodoistAPI
 import time
 import schedule
-import os 
+import os
 
-def fetch_classes(): return [t for t in TodoistAPI("").get_tasks() if t.due and t.due.date==time.strftime("%Y-%m-%d") and len(t.content)==4 and " " in t.content and ("P" in t.content or "L" in t.content)]
+def fetch_classes(): return [t for t in TodoistAPI("e3b0b2ed0642281f8f775fc954ef1567ea84537c").get_tasks() if t.due and t.due.date==time.strftime("%Y-%m-%d") and len(t.content)==4 and " " in t.content and ("P" in t.content or "L" in t.content)]
 
 def get_preponed_time(class_time:str): return f"{class_time.split(":")[0]}:{int(class_time.split(":")[-1])-3}"
 
 def open_class(class_data):
-    class_type,class_name,class_time=class_data
+    import pyperclip
+    class_type,class_name=class_data
     d=get_classes_data()[class_name][class_type]
     class_link,class_pin=d.get("uri"),d.get("pin")
     os.system(f"start \"\" {class_link}")
@@ -20,18 +20,14 @@ def get_classes_data():
     with open(os.path.join(os.path.dirname(os.path.abspath(__file__)),"data.json"),encoding="utf-8") as f:
         return json.load(f)
 
-def schedule_classes(classes):
-    for t in classes:
+def schedule_classes():
+    for t in fetch_classes():
         class_type,class_name=t.content.split()
         class_time=t.due.string.split()[-1]
-        
-        preponed_time=get_preponed_time(class_time)
-        schedule.every().day.do(open_class,(class_type,class_name,preponed_time))
+        print(f"{'Lekcija' if class_type=='L' else 'Praktyka'} {class_name} o {class_time}")
+        schedule.every().day.at(get_preponed_time(class_time)).do(open_class,(class_type,class_name))
 
-""" 
+schedule_classes()
 while 1:
     schedule.run_pending()
     time.sleep(40)
-"""
-
-open_class(("L","PP","16:42"))
