a1c7247fe55a27fa48b79fb9217ab09572d32fe9
Oleh Onyshchenko <seesmwork@gmail.com>
2024-11-04 12:54:40 +0200

    feat: add task class for easier properties access

--- Biblija.py
+++ Biblija.py
@@ -8,17 +8,27 @@ parent_id=None
 Bible_tasks=todoist_api.get_tasks()
 check_tasks=[t for t in Bible_tasks if f'Day {day_number}' in t.content]
 
+class Task:
+    def __init__(
+        self,
+        content:str,
+        due:str=None,
+        project_id:int=target_project,
+        parent_id:int=None
+    ):
+        self.content=content
+        self.due=due
+        self.project_id=project_id
+        self.parent_id=parent_id
+
 if not check_tasks:
-    task_details={
-        "content":"Day 67: Luke 24, Exodus 18, Proverbs 5",
-        "due_string":"today",
-        "project_id":target_project
-    }
+    details=Task("Day 67","today")
     new_task=todoist_api.add_task(
-        content=task_details['content'],due_string=task_details['due_string'],project_id=task_details['project_id']
+        content=details.content,due_string=details.due,project_id=details.project_id
     )
     parent_id=new_task.id
 else:
     parent_id=check_tasks[0].id
 
+day_details="Luke 24, Exodus 18, Proverbs 5".split(", ")
 print(parent_id)
