a124670267865789a40755de91f576f4daea24b4
Oleh Onyshchenko <seesmwork@gmail.com>
2024-11-05 12:22:05 +0200

    feat: JESUS THANK YOU LORD ДЯЮКУ ІСУСЕ ХРИСТЕ СВЯТИЙ БОЖЕНЬКО БЛАГИЙ АМІНь

--- Biblija.py
+++ Biblija.py
@@ -2,6 +2,8 @@ import os
 import time
 from const import *
 
+all_tasks=todoist_api.get_tasks()
+
 class Task:
     def __init__(
         self,
@@ -9,18 +11,16 @@ class Task:
         due: str = None,
         parent: int = None,
         project: str = str(2342885371),
-        description:str=None
     ):
         self.content = content
         self.due = due
         self.project_id = project
         self.parent_id = parent
-        self.description=description
 
 def safely_add_task(
     task_name: str,
     task_details: Task,
-    search_tasks: list = todoist_api.get_tasks()
+    search_tasks: list = all_tasks
 ):
     check_for_task = [t for t in search_tasks if task_name in t.content]
     if not check_for_task:
@@ -29,7 +29,6 @@ def safely_add_task(
             due_string=task_details.due,
             project_id=task_details.project_id,
             parent_id=task_details.parent_id,
-            description=task_details.description
         )
     else:
         target_task = check_for_task[0]
@@ -38,49 +37,35 @@ def safely_add_task(
 def form_reading_link(Book:int,chapter:int): 
     return f"https://ebible.org/study/?w1=bible&t1=local%3Aukr1871&v1={tiny_abbreviations[Book]}{chapter}"
 
-"""
 with open(os.path.join(os.path.dirname(os.path.abspath(__file__)),"readings_cache.txt")) as f:
-    data=[l.strip() for l in f.readlines()]
-    day,lists=data[0],data[1:]
+    target_day=int(f.read())
 
-day_number = 67
-day_task = safely_add_task(f"Day {day_number}", Task("Day 67", "today"))
+subtask_details=[]
+for list_index in range(10):
+    Book_number,chapter_number=0,0
+    Books_data=Book_lists[list_index]
+    selected_Book=Books_data[Book_number]
+    current_day=0
+    while current_day!=target_day:
+        if chapter_number<chapter_counts[selected_Book]:chapter_number+=1
+        else:
+            Book_number=Book_number+1 if Book_number<len(Books_data)-1 else 0
+            chapter_number=1
+        selected_Book=Books_data[Book_number]
+
+        current_day+=1
+        if current_day==target_day:
+            reading_url=form_reading_link(selected_Book,chapter_number)
+            subtask_details.append((f'{Book_names_ua[selected_Book]} {chapter_number}',reading_url))
+
+task_name=f"Біблія {target_day}"
+day_task = safely_add_task(f"Біблія {target_day}", Task(task_name, "today"))
 parent_id = day_task.id
 
-day_details = "Luke 24, Exodus 18, Proverbs 5".split(", ")
-for list_number,list_name in enumerate(day_details):
-    new_sub_task = safely_add_task(
-        list_name,
-        Task(f'**{list_number+1}** {list_name}', parent=parent_id),
-        [t for t in all_tasks if t.parent_id == parent_id],
+for list_index,list_data in enumerate(subtask_details):
+    reading_details,reading_link=list_data
+    reading_subtask=safely_add_task(
+        reading_details,
+        Task(f'[{reading_details}]({reading_link})',parent=parent_id),
+        [t for t in all_tasks if t.parent_id==parent_id]
     )
-"""
-
-td=68
-cd=0
-lists=[
-(0,0),
-(0,0),
-(0,0),
-(0,0),
-(0,0),
-(0,0),
-(0,0),
-(0,0),
-(0,0),
-(0,0),
-]
-bn=0
-cn=0
-sl=Book_lists[3]
-sb=sl[bn]
-while cd!=td:
-    cd+=1
-    if cn<chapter_counts[sb]:cn+=1
-    else:
-        bn=bn+1 if bn<len(sl)-1 else 0
-        cn=1
-    sb=sl[bn]
-
-    if cd==td:
-        print(Book_names_ua[sb],cn)
