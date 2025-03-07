3324ab0c8ee86bd3ce26410431f24d335e73776d
Oleh Onyshchenko <seesmwork@gmail.com>
2025-03-07 11:43:15 +0200

    open para once again at class time

--- run.py
+++ run.py
@@ -21,6 +21,18 @@ def reschedule_paras():
     schedule.clear()
     schedule_paras()
 
+
+def get_scheduled_paras():
+    return [
+        task for task in todoist_api.get_tasks()
+        if task.due
+        and task.due.date == time.strftime("%Y-%m-%d")
+        and len(task.content) == 4
+        and " " in task.content
+        and ("P" in task.content.upper() or "L" in task.content.upper())
+    ]
+
+
 def schedule_paras():
     scheduled_paras = get_scheduled_paras()
     if not scheduled_paras:
@@ -37,38 +49,34 @@ def schedule_paras():
         para_kind, para_name = para_kind.upper(), para_name.upper()
         para_scheduled_time = para_data.due.string.split()[-1]
         print(f"{lib.get_full_para_kind(para_kind)} {para_name} o {para_scheduled_time}")
-        earlier_time=get_earlier_time(para_scheduled_time)
-        schedule.every().day.at(earlier_time).do(open_para, f'{para_kind} {para_name}')
 
-def get_scheduled_paras():
-    return [
-        task for task in todoist_api.get_tasks()
-        if task.due
-        and task.due.date == time.strftime("%Y-%m-%d")
-        and len(task.content) == 4
-        and " " in task.content
-        and ("P" in task.content.upper() or "L" in task.content.upper())
-    ]
+        earlier_time: str = get_earlier_time(para_scheduled_time)
+        para_abbreviation: str = f"{para_kind} {para_name}"
+        schedule.every().day.at(earlier_time).do(open_para, para_abbreviation)
+        schedule.every().day.at(para_scheduled_time).do(open_para, para_abbreviation)
+
 
 def open_para(para_abbr: str = "L IV"):
-    this_class_data=[d for d in make_paras_data() if d.name==para_abbr][0]
+    this_class_data: ParaData = [d for d in make_paras_data() if d.name==para_abbr][0]
 
     os.system(f'start "" {this_class_data.link}')
     lib.copy_text(this_class_data.code) if this_class_data.code else None
     try:
         recorder.start_record()
     except:
-        recorder.stop_record()
-        recorder.start_record()
+        pass
 
-    def get_time(m):
+    def get_time(m) -> str:
         return (datetime.datetime.now() + datetime.timedelta(minutes=m)).strftime('%H:%M')
 
-    CLASS_DURATION_MINUTES=80
-    HOW_LONG_TO_RECORD_FOR=CLASS_DURATION_MINUTES+10
-    t=get_time(HOW_LONG_TO_RECORD_FOR)
-    schedule.every().day.at(t).do(close_para,para_abbr)
-    schedule.every().day.at(t).do(reschedule_paras)
+    CLASS_DURATION_MINUTES: int = 80
+    HOW_LONG_TO_RECORD_FOR: int = CLASS_DURATION_MINUTES+10
+
+    time_difference_from_now: str = get_time(HOW_LONG_TO_RECORD_FOR)
+    schedule.every().day.at(time_difference_from_now).do(close_para,para_abbr)
+    schedule.every().day.at(time_difference_from_now).do(reschedule_paras)
+
+
 
 def move_recording_to_its_folder(para_abbr: str = "L IV"):
     def get_latest_file():
@@ -84,6 +92,7 @@ def move_recording_to_its_folder(para_abbr: str = "L IV"):
     moved_file_path=os.path.join(lib.recordings_folder,name,kind,file_name)
     os.replace(latest_file_path,moved_file_path)
 
+
 def close_para(para_abbr: str = "L IV"):
     try:
         recorder.stop_record()
@@ -91,6 +100,7 @@ def close_para(para_abbr: str = "L IV"):
     except:
         print('Failed to stop recording')
     move_recording_to_its_folder(para_abbr)
+
 
 def make_paras_data():
     target_file_path=os.path.join(r"E:\Notatnyk\Університет\20250130174547 32 Інформація про курси - посилання на заняття, список завдань та викладачів з дисциплін.md")
@@ -101,10 +111,11 @@ def make_paras_data():
 
     for line in lines:
         clean_line=line[2:].strip()
-        para=ParaData(*clean_line.split(' - '))
+        para: ParaData = ParaData(*clean_line.split(' - '))
         paras.append(para)
 
     return paras
+
 
 def main():
     reschedule_paras()
